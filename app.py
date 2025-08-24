bfc51933647b221b42c1822df0c61528883d6d7a
Abdul Hameed T A <hameedamithattuparambil@gmail.com>
2025-08-24 21:56:12 +0530

    made it dynamic using AJAX and JSON

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -8,25 +8,36 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-@app.route("/", methods = ["GET", "POST"])
+@app.route("/")
 def home():
+    return render_template("index.html")
 
-    if request.method == "POST":
-        name = request.form.get("name")
-        message = request.form.get("message")
-
-        conn = get_db_connection()
-        conn.execute("INSERT INTO notes (name, message) VALUES (?, ?)", (name, message))
-        # conn.execute("DELETE FROM notes")
-        conn.commit()
-        conn.close()
-
-
+@app.route("/api/notes")
+def api_notes():
     conn = get_db_connection()
     notes = conn.execute("SELECT * FROM notes ORDER BY created_at DESC").fetchall()
     conn.close()
 
-    return render_template("index.html", notes = notes)
+    notes_list = [dict(note) for note in notes]
+    return jsonify(notes_list)
+
+@app.route("/api/add_note", methods=["POST"])
+def api_add_note():
+        data = request.json
+        name = data.get("name")
+        message = data.get("message")
+
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute("INSERT INTO notes (name, message) VALUES (?, ?)", (name, message))
+        # conn.execute("DELETE FROM notes")
+        conn.commit()
+
+        note_id = cur.lastrowid
+        note = conn.execute("SELECT * FROM notes WHERE id = ?", (note_id,)).fetchone()
+        conn.close()
+
+        return jsonify(dict(note))
 
 if __name__ == "__main__":
     app.run(debug=True)
