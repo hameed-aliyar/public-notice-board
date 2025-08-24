3cda64cbe3995647ac3cca61e1ba6838d6a0f3f1
Abdul Hameed T A <hameedamithattuparambil@gmail.com>
2025-08-24 15:26:24 +0530

    added a form for inputs

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ["GET", "POST"])
 def home():
-    notes = ["Hello", "Welcome", "First Note!", "No way"]
+    notes = ["Hello", "Welcome", "First Note!", "No way", "Go away"]
+
+    if request.method == "POST":
+        name = request.form.get("name")
+        message = request.form.get("message")
+        print(f"New note from {name} : {message}.")
+
     return render_template("index.html", notes = notes)
 
 if __name__ == "__main__":
